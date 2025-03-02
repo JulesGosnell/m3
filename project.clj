ab6c44ed164be6840c86a9b5b46ce41c0e4094fb
jules <jafg@agordcm.com>
2025-03-02 00:05:26 +0000

    update versions

--- project.clj
+++ project.clj
@@ -5,18 +5,16 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies
-  [[org.slf4j/slf4j-simple "2.0.16"]                                        ;; https://github.com/qos-ch/slf4j
-   [org.graalvm.polyglot/polyglot "24.1.0"]
-   [org.graalvm.polyglot/js "24.1.0" :extension "pom"]
+  [[org.slf4j/slf4j-simple "2.0.17"]                                        ;; https://github.com/qos-ch/slf4j
+   [org.graalvm.polyglot/polyglot "24.1.2"]                                 ;; https://github.com/oracle/graal
+   [org.graalvm.polyglot/js "24.1.2" :extension "pom"]                      ;; https://github.com/oracle/graal
    [com.fasterxml.jackson.core/jackson-core "2.18.2"]                       ;; https://github.com/FasterXML/jackson-core
-   [cheshire/cheshire "5.13.0"]                                             ;; [cheshire/cheshire "5.13.0"]
-
    [org.clojure/clojure "1.12.0"]                                           ;; https://clojure.org/releases/downloads
    [org.clojure/clojurescript "1.11.132" :scope "provided"]                 ;; https://github.com/clojure/clojurescript
    [org.clojure/tools.logging "1.3.0"]                                      ;; https://github.com/clojure/tools.logging
-
-   [com.widdindustries/cljc.java-time "0.1.21"]                             ;; https://github.com/henryw374/cljc.java-time
    [camel-snake-kebab "0.4.3"]                                              ;; https://github.com/clj-commons/camel-snake-kebab
+   [cheshire/cheshire "5.13.0"]                                             ;; https://github.com/dakrone/cheshire
+   [com.widdindustries/cljc.java-time "0.1.21"]                             ;; https://github.com/henryw374/cljc.java-time
    ]
 
   :jvm-opts ["-Xmx4G"]
